91d4f8daca1aa48f70392f4ddda59c854c61cd9e
Silin Na <n1207n@gmail.com>
2023-06-16 21:39:51 -0700

    feat: Introduced ID_SIZE constant for test ID generations

--- cmd/driver.go
+++ cmd/driver.go
@@ -2,10 +2,12 @@ package main
 
 import snowflake "github.com/n1207n/golang-snowflake-id-generator"
 
+const ID_SIZE = 1_000_000
+
 func main() {
 	worker := snowflake.NewSnowflakeWorker(1)
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < ID_SIZE; i++ {
 		println(worker.NextID())
 	}
 }
